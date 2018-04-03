369083e7182e6d4257b7e6920642390b3f5cda1e
QA Instructor <QAInstructorSky@gmail.com>
2018-04-03 18:31:33 +0100

    edits to index display text

--- index.php
+++ index.php
@@ -11,12 +11,10 @@
     </head>
     <body>
         <h1>Main Heading</h1>
-
         <h2>An H2 heading</h2>
-
-        <h3>some h3 stuff>/h3>
-
-
+        <h3>some h3 stuff</h3>
+        <h4>some h4 stuff</h4>
+        <h5>some h5 stuff</h5>
         <h6>An H6 Heading</h6>
 
     </body>
